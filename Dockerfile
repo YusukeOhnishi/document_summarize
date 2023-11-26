f75ce4b13aaf900b5a0efa2f16995164123de1ce
Takenori Kusaka <kokorokagami@gmail.com>
2023-11-26 16:38:02 +0900

    OpenAIのclientを作成する際にazure版でも作れるように拡張

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,10 @@ COPY ./ /app/
 RUN apt-get update && apt-get install -y curl
 RUN pip install -r requirements.txt
 # Set your OpenAI API Key
-ENV OPENAI_API_KEY 'Your OpenAI API Key'
+ENV API_TYPE 'Your OpenAI API Type'
+ENV API_KEY 'Your OpenAI API Key'
+ENV API_ENDPOINT 'Your OpenAI API Endpoint'
+ENV API_VERSION 'Your OpenAI API Version'
 
 EXPOSE 8501
 HEALTHCHECK CMD curl --fail http://localhost:8501/_stcore/health
--- src/chat_model.py
+++ src/chat_model.py
@@ -5,7 +5,27 @@ import tiktoken
 
 class ChatModel():
     def __init__(self, model_name):
-        self.client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+        # Azure OpenAI Service のエンドポイントと API キーを設定します。
+        api_type = os.getenv('API_TYPE')
+        api_key = os.getenv('API_KEY')
+        endpoint = os.getenv('API_ENDPOINT')
+        api_version = os.getenv('API_VERSION')
+
+        if api_type == 'azure':
+            self.client = OpenAI(
+                api_endpoint=endpoint,
+                api_key=api_key,
+                api_version=api_version
+            )
+        elif api_type == 'openai':
+            self.client = OpenAI(
+                api_key=api_key
+            )
+        else:
+            if api_type:
+                raise Exception('API_TYPE is not azure or openai.')
+            else:
+                raise Exception('API_TYPE is not set.')
         self.model_name = model_name
 
     def get_chat_message(self, message):
